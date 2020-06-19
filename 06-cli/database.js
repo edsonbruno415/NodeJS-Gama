5787bb2d43430544b823d85c22b9d76bbb541011
Edson Bruno <edsonbruno415@gmail.com>
2020-06-19 16:49:10 -0300

    Finalizado os testes no CRUD de arquivos

--- 06-cli/database.js
+++ 06-cli/database.js
@@ -1,10 +1,12 @@
 const { 
-    readFile
+    readFile,
+    writeFile
 } = require('fs');
 
 const { promisify } = require('util');
 
 const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
 
 // outra forma de obter dados do JSON
 // const dadosJSON = require('./herois.json');
@@ -19,10 +21,66 @@ class Database {
         return JSON.parse(arquivo.toString());
     }
 
-    excreverArquivo(){
-
+    async escreverArquivo(dados){
+        await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados));
+        return true;
     }
 
+    async cadastrar(heroi){
+        const dados = await this.obterDadosArquivo();
+        const id = heroi.id <= 2 ? heroi.id : Date.now();
+
+        const heroiComId = {
+            id,
+            ...heroi
+        }
+
+        const dadosFinal = [
+            ...dados,
+            heroiComId
+        ]
+
+        const resultado = await this.escreverArquivo(dadosFinal);
+
+        return resultado;
+    }
+
+    async atualizar(id, modificacoes){
+        const dados = await this.obterDadosArquivo();
+        const indice = dados.findIndex( item => item.id === parseInt(id));
+
+        if(indice === -1){
+            throw Error('O heroi informado nao existe');
+        }
+
+        const atual = dados[indice];
+        const objetoAtualizado = {
+            ...atual,
+            ...modificacoes
+        }
+        dados.splice(indice, 1);
+
+        return await this.escreverArquivo([
+            ...dados,
+            objetoAtualizado
+        ]);
+    }
+
+    async remover(id){
+        if(!id){
+            return await this.escreverArquivo([]);
+        }
+
+        const dados = await this.obterDadosArquivo();
+        const indice = dados.findIndex( item => item.id === parseInt(id) );
+        if(indice === -1){
+            throw Error('O Heroi informado nao existe');
+        }
+
+        dados.splice(indice, 1);
+
+        return await this.escreverArquivo(dados);
+    }
     async listar(id){
         const dados = await this.obterDadosArquivo();
         const dadosFiltrados = dados.filter( item => id ? item.id === id : true);
