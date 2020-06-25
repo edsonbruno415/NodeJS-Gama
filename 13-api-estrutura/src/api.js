28fed5623b73984cd750a88635ebdc7c16a7422d
Edson Bruno <edsonbruno415@gmail.com>
2020-06-25 17:57:00 -0300

    Criando e testando a api Herois

---
+++ 13-api-estrutura/src/api.js
@@ -0,0 +1,28 @@
+ //npm install hapi
+const Hapi = require('hapi');
+const HeroiSchema = require('./db/strategies/mongodb/schemas/heroisSchema');
+const MongoDb = require('./db/strategies/mongodb/mongodb');
+const Context = require('./db/strategies/base/contextStrategy');
+const app = new Hapi.Server({
+    port: 5000
+});
+
+async function main(){
+    const connection = MongoDb.connect();
+    const context = new Context(new MongoDb(connection, HeroiSchema));
+
+    app.route([{
+        path: '/herois',
+        method: 'GET',
+        handler: (request, handler)=>{
+            return context.read();
+        }
+    }]);
+
+    await app.start();
+    console.log('Application is running on port ',app.info.port);
+
+    return app;
+}
+
+module.exports = main();
