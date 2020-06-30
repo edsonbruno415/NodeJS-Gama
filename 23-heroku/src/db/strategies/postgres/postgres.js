c4ab0b5defa55854dd7b852d9c5d3aff3944b30a
Edson Bruno <edsonbruno415@gmail.com>
2020-06-30 00:51:57 -0300

    Adicionando env POSTGRES_URL

--- 23-heroku/src/db/strategies/postgres/postgres.js
+++ 23-heroku/src/db/strategies/postgres/postgres.js
@@ -1,36 +1,36 @@
 const ICrud = require('../interfaces/interfaceCrud');
 const Sequelize = require('sequelize');
 
-class Postgres extends ICrud{
-    constructor(connection, schema){
+class Postgres extends ICrud {
+    constructor(connection, schema) {
         super();
         this._connection = connection;
         this._schema = schema;
     }
 
-    async isConnected(){
-        try{
+    async isConnected() {
+        try {
             await this._connection.authenticate();
             return true;
         }
-        catch(error){
+        catch (error) {
             console.error('fail', error);
             return false;
         }
     }
 
-    static async defineModel(connection, schema){
+    static async defineModel(connection, schema) {
         const model = connection.define(
             schema.name,
             schema.schema,
             schema.options
         );
-    
+
         await model.sync();
         return model;
     }
 
-    static async connect(){
+    static async connect() {
         const connection = new Sequelize(process.env.POSTGRES_URL, {
             host: 'localhost',
             dialect: 'postgres',
@@ -38,7 +38,10 @@ class Postgres extends ICrud{
             logging: false,
             ssl: process.env.SSL_DB,
             dialectOptions: {
-                ssl: process.env.SSL_DB
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+                }
             }
             //operatorsAliases: false
         });
@@ -46,24 +49,24 @@ class Postgres extends ICrud{
         return connection;
     }
 
-    async create(item){
+    async create(item) {
         const { dataValues } = await this._schema.create(item);
 
         return dataValues;
     }
 
-    read(item){
-        return this._schema.findAll({ where: item, raw: true});
+    read(item) {
+        return this._schema.findAll({ where: item, raw: true });
     }
 
-    update(id, item, upsert = false ){
+    update(id, item, upsert = false) {
         const fn = upsert ? 'upsert' : 'update';
 
 
-        return this._schema[fn](item, { where: { id : id }});
+        return this._schema[fn](item, { where: { id: id } });
     }
 
-    delete(id){
+    delete(id) {
         const query = id ? { id } : {};
         return this._schema.destroy({ where: query });
     }
